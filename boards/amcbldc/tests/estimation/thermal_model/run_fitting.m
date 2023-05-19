c84db1e2042c68d92377f6c7937fa6a0d8c8918f
Mattia Fussi <mattia.fussi@iit.it>
2023-05-19 10:11:53 +0200

    add scripts to fit thermal model

--- boards/amcbldc/tests/estimation/thermal_model/run_fitting.m
+++ boards/amcbldc/tests/estimation/thermal_model/run_fitting.m
@@ -1,21 +1,38 @@
+%% Thermal Model Fit and Design
 % Copyright (C) 2023 Fondazione Istitito Italiano di Tecnologia (IIT)
+% 
 % All Rights Reserved.
-%
+%% Read datasets
+% Training set
 
-close all
-
-training_measurements = "4.xlsx";
-training_input = "n4.mat";
-training_start_time_offset = 827;
-
-validation_measurements = "1.xlsx";
-validation_input = "n1.mat";
-validation_start_time_offset = 1;
-
-[idd_training] = create_iddata(training_measurements, training_input, training_start_time_offset);
-[idd_validation] = create_iddata(validation_measurements, validation_input, validation_start_time_offset);
+training_measurements = "0516/1.xlsx";
+training_input = "0516/n1.mat";
+training_start_time_offset = 10000;
+training_Ts = 0.01;
+motor_ID = 3; % We assume that we gathered data where we logged 3 motors
+[idd_training] = create_iddata(training_measurements, ...
+                               training_input, training_start_time_offset, training_Ts, motor_ID);
 idd_training.Name = "training";
-idd_validation.Name = "validation";
+% Validation sets
+% The model fitted with training data will be tested against more than one validation 
+% set
+
+n_validation_sets = 3;
+idd_validations = cell(n_validation_sets, 1);
+
+validation_measurements = {"0516/4.xlsx", "0516/3.xlsx", "0516/2.xlsx"};
+validation_inputs = {"0516/n4.mat", "0516/n3.mat", "0516/n2.mat"};
+
+
+for i=1:n_validation_sets    
+    validation_start_time_offset = 1;
+    validation_Ts = 0.01;
+    idd_validations{i} = create_iddata(validation_measurements{i}, ...
+                                     validation_inputs{i}, validation_start_time_offset, validation_Ts, motor_ID);
+    idd_validations{i}.Name = "validation";
+end
+% Plot sets
+
 figure
 tiledlayout(2,2, "TileSpacing","tight","TileIndexing","columnmajor")
 h1 = nexttile;
@@ -25,48 +42,60 @@ h2 = nexttile;
 plot(idd_training.OutputData)
 ylabel("Training Output (C)")
 linkaxes([h1 h2], 'x')
-h1 = nexttile;
-plot(idd_validation.InputData, "LineWidth", 1.5)
+h3 = nexttile;
+plot(idd_validations{1}.InputData, "LineWidth", 1.5)
 ylabel("Validation input (W)")
-h2 = nexttile;
-plot(idd_validation.OutputData)
+h4 = nexttile;
+plot(idd_validations{1}.OutputData)
 ylabel("Validation Output (C)")
-linkaxes([h1 h2], 'x')
-
-%% Run estimation: continuous LTI model with 1 pole
+linkaxes([h3 h4], 'x')
+ylimsO = [min(h2.YLim(1), h4.YLim(1)),max(h2.YLim(2), h4.YLim(2)) * 1.0];
+h2.YLim = ylimsO;
+h4.YLim = ylimsO;
+ylimsI = [min(h1.YLim(1), h3.YLim(1)), max(h1.YLim(2), h3.YLim(2)) * 1.0];
+h1.YLim = ylimsI;
+h3.YLim = ylimsI;
+%% Run model fitting
+% We target a continuous LTI model with 1 pole:
+% 
+% $$\frac{\Delta T}{P} = \frac{R_T}{ \tau \cdot s+ 1}$$
+% 
+% where the fitted parameters are
+% 
+% $R_T$ = thermal resistance
+% 
+% $\tau$ = thermal time constant
 
 opt = procestOptions;
 opt.InitialCondition =  "zero";
 model = procest(idd_training, "P1", opt);
+%% Plot results
 
-%% Plot result
+[rmse, fig_handle] = plot_results(model, idd_training, idd_validations, n_validation_sets, training_Ts, validation_Ts);
 
-figure
-subplot(2,1,1)
-timevec = 0:0.1:((length(idd_training.InputData)-1)/10);
-[y_out, time] = lsim(model, idd_training.InputData, timevec);
-plot(time, y_out, "LineWidth", 1.5)
-hold on
-plot(timevec, idd_training.OutputData, "LineWidth", 1.5);
-ylabel("\Delta T (C)")
-legend({"model", "camera"})
-title("Training data")
-h1 = gca;
-
-subplot(2,1,2)
-timevec = 0:0.1:((length(idd_validation.InputData)-1)/10);
-[y_out, time] = lsim(model, idd_validation.InputData(1:end), timevec(1:end));
-plot(time, y_out, "LineWidth", 1.5)
-ylabel("\Delta T (C)")
-xlabel("Time (sec)")
-hold on
-plot(timevec(1:end), idd_validation.OutputData(1:end), "LineWidth", 1.5);
-title("Validation data")
-h2 = gca;
-
-linkaxes([h1, h2], 'x')
-sgtitle("1Kg - training with no motion, validation with motion")
-rmse = sqrt(mean((y_out - idd_validation.OutputData).^2))
-
-% exportgraphics(gcf, "1.5kg-1Kgno.png")
+% exportgraphics(fig_handle, "results.png")
 % print -clipboard -dbitmap
+%% Model discretization
+% The resulting model is here discretized with Tustin. The parameters used also 
+% involve the phase resistance $R_T$, which is used to compute the dissipated 
+% power in the architectural model:
+% 
+% $$P=R_T \cdot \;\left(I_{q,\;\mathrm{RMS}}^2 +I_{d,\;\mathrm{RMS}}^2 \right)$$
+
+% Symbolic variables
+syms R_L R_T T_s tau z
+s = sym('s');
+
+% Numerical values
+RlV = single(25.9);  
+RtV = single(model.Kp)
+tauV = single(model.Tp1)
+TsV = 1e-2;
+
+continuous_model_sym = R_L * R_T / (tau * s + 1)
+
+tustin_conversion = 2/T_s * (z - 1)/(z+1);
+
+discrete_model_sym = simplifyFraction(subs(continuous_model_sym, s, tustin_conversion))
+
+discrete_model = simplify(subs(discrete_model_sym, [R_L R_T T_s tau], [RlV RtV TsV tauV]));
