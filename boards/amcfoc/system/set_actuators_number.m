ed6f10d06dce9894db23103c2db97fb70ed646d6
Mattia Fussi <mattia.fussi@iit.it>
2023-10-20 15:41:53 +0200

    Improve configurability of amcfoc

--- boards/amcfoc/system/set_actuators_number.m
+++ boards/amcfoc/system/set_actuators_number.m
@@ -1,24 +1,48 @@
+% Copyright (C) 2022 Fondazione Istitito Italiano di Tecnologia (IIT)
+% All Rights Reserved.
 
-% This script should be ran only once, after definition of which
-% parameters need to be vectorized
+function set_actuators_number
+    % This script should be ran only once, after definition of which
+    % parameters need to be vectorized
 
-% Get reference to design data of dictionary
-dict = Simulink.data.dictionary.open("dictionaries/amcfoc.sldd");
-dd = dict.getSection("Design Data");
+    % Here you need to list the variables that you want to vectorize
+    entries_to_modify = [
+        "Actuators" 
+        "InitConfParams"
+        ];
 
-% find desired number of actuators
-numOfActuators = dd.getEntry("numberOfActuators").getValue;
+    % Get reference to design data of dictionary
+    dict = Simulink.data.dictionary.open("amcfoc.sldd");
+    dd = dict.getSection("Design Data");
+    
+    % find desired number of actuators
+    numOfActuators = dd.getEntry("numberOfActuators").getValue;
+      
+    for i=1:length(entries_to_modify)
+    
+        % get reference to entry to modify the dimension
+        e = dd.getEntry(entries_to_modify(i));
 
-entries_to_modify = ["Actuators", "InitConfParams"];
+        % get reference to the value of the entry
+        simpar = e.getValue;
 
-for i=1:length(entries_to_modify)
+        if(~isempty(simpar.Value))
+            warning("The field 'Value' of " + entries_to_modify(i) + " is not empty.");
+            reply = input("Would you like to clear " + entries_to_modify(i) + "? " + ...
+                    "Choosing No will skip the entry. [Y/n]", "s");
+            
+            if(lower(reply) == "n")
+                continue;
+            else
+                simpar.Value = [];
+            end
+        end
 
-    % get reference to entry to modify the dimension
-    e = dd.getEntry(entries_to_modify(i));
-    % modify dimension and write it to referenced parameter
-    simpar = e.getValue;
-    simpar.Dimensions = [1, numOfActuators];
-    e.setValue(simpar);
-
+        % modify dimension and write it to referenced parameter
+        simpar.Dimensions = [1, numOfActuators];
+        e.setValue(simpar);
+    
+    end
+    saveChanges(dict);
+    disp("Changes saved to dictionary.");
 end
-dict.saveChanges;
