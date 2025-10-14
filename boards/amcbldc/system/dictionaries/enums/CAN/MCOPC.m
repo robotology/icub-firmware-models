7c0b131688c597dee5878c26dd31c61a32222f27
Raffaele Pumpo <raffaele.pumpo@iit.it>
2025-10-14 11:11:45 +0200

    Add position  Pid and Position target

--- boards/amcbldc/system/dictionaries/enums/CAN/MCOPC.m
+++ boards/amcbldc/system/dictionaries/enums/CAN/MCOPC.m
@@ -4,10 +4,12 @@
 classdef MCOPC < Simulink.IntEnumType
     enumeration
         Set_Parameter(0x01)
+        Set(0x05)
         Set_Control_Mode(0x09)
         Set_Current_Limit(0x48)
         Set_Current_PID(0x65)
         Set_Velocity_PID(0x69)
+        Set_Position_PID(0x52)
         Set_Motor_Config(0x77)
     end
     methods (Static = true)
